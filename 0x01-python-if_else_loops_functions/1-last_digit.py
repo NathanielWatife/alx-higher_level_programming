749903af25abb0729c2f7d0ca3b85416a72bdaa1
NathanielWatife <awosanminathaniel0@gmail.com>
2023-10-04 03:32:09 +0100

    new update

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-# YOUR CODE HERE
-last_digit = number % 10
-
-
-# Check if the last digit is greater than 5
-if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5", end=" ")
-# Check if the last digit is 0
-elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0", end=" ")
-# Otherwise, the last digit is less than 6 and not 0
+digit = abs(number) % 10
+if number < 0:
+    digit = -digit
+print("Last digit of {} is {} and is ".format(number, digit), end="")
+if digit > 5:
+    print("greater than 5")
+elif digit == 0:
+    print("0")
 else:
-    print(f"Last digit of {number} is {last_digits} and is less than 6 and not 0", end=" ")
-    
+    print("less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
-# loop through the ASCII values of lowercase letters
-for char in range(ord('a'), ord('z') + 1):
-    if char != ord('e') and char != ord('q'):
-        print(chr(char), end=" ")
+#!/usr/bin/python3
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
