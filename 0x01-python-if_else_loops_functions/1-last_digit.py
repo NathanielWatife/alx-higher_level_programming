44c18035170e85a931ddfbb30db15e0368519e60
NathanielWatife <awosanminathaniel0@gmail.com>
2023-10-04 03:39:24 +0100

    solved

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,3 +11,15 @@ elif digit == 0:
     print("0")
 else:
     print("less than 6 and not 0")
+# YOUR CODE HERE
+last_digit = number % 10
+
+# Check if the last digit is greater than 5
+if last_digit > 5:
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+# Check if the last digit is 0
+elif last_digit == 0:
+    print(f"Last digit of {number} is {last_digit} and is 0")
+# Otherwise, the last digit is less than 6 and not 0
+else:
+    print(f"Last digit of {number} is -{last_digits} not is less than 6and not 0")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,3 +2,7 @@
 for letter in range(97, 123):
     if chr(letter) is not 'q' and chr(letter) is not 'e':
         print("{}".format(chr(letter)), end="")
+# loop through the ASCII values of lowercase letters
+for char in range(ord('a'), ord('z') + 1):
+    if char != ord('e') and char != ord('q'):
+        print(chr(char), end=" ")
