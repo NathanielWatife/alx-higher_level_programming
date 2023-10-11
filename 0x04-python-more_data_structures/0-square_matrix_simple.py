654cd5acc7dd93e420563b5a44e5a04e9020d9e4
NathanielWatife <awosanminathaniel0@gmail.com>
2023-10-12 00:31:23 +0100

    new dircetory aand files created with a README.md file

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+# Computes the square value of all integers of a matrix.
+# Args:
+# matrix: A 2-dimensional array of integers.
+# Returns:
+# A new 2-dimensional array of the same size as `matrix`, where each value is the
+# square of the corresponding value in `matrix`.
+# Create a new matrix to store the squared values.
+    squared_matrix = []
+    for row in matrix:
+        squared_row = []
+    for value in row:
+        squared_row.append(value ** 2)
+    squared_matrix.append(squared_row)
+    return squared_matrix
