596b5fa0df1f8679d440b9756cbb88ebe327da85
NathanielWatife <awosanminathaniel0@gmail.com>
2023-10-11 16:41:01 -0700

    executable

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-# Computes the square value of all integers of a matrix.
-# Args:
-# matrix: A 2-dimensional array of integers.
-# Returns:
-# A new 2-dimensional array of the same size as `matrix`, where each value is the
-# square of the corresponding value in `matrix`.
-# Create a new matrix to store the squared values.
+    """Computes the square value of all integers of a matrix.
+    Args:
+    matrix: A 2-dimensional array of integers.
+    Returns:
+    A new 2-dimensional array of the same size as `matrix`, where each value is
+    the square of the corresponding value in `matrix`.
+    """
+    # Create a new matrix to store the squared values.
     squared_matrix = []
     for row in matrix:
         squared_row = []
