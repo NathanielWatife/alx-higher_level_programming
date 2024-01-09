146f6cae53adaadddb5b8fd5783a5dd4e30e4024
NathanielWatife <awosanminathaniel0@gmail.com>
2024-01-09 11:01:12 +0100

    6-multi-langauges_loop.js file added

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const multiLine = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+let i = 0;
+
+while (i < multiLine.length) {
+  console.log(multiLine[i]);
+  i++;
+}
