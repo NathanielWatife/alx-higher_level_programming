da8a19af8f61937d21a4062251b3f7a803305a20
NathanielWatife <awosanminathaniel0@gmail.com>
2024-01-09 01:53:01 +0100

    new files added

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const fistLine = "C is fun";
+const secondLin = "Python is cool";
+const thirdLine = "JavaScript is amazing";
+
+console.log(fistLine)
+console.log(secondLin)
+console.log(thirdLine)
