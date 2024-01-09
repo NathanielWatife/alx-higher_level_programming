71c0a851bba07f021f5ca64730dc3a6c1f438d55
NathanielWatife <awosanminathaniel0@gmail.com>
2024-01-09 11:07:34 +0100

    7-multi.js worked on again

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,8 +6,8 @@ const myInt = parseInt(myArg, 10);
 if (!isNaN(myInt)) {
   let i = 0;
   while (i < myInt) {
-	console.log('C is fun');
-	i++;
+    console.log('C is fun');
+    i++;
   }
 } else {
   console.log('Missing number of occurrences');
