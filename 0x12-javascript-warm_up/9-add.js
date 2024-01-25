7e645eb22fc9d39e171070534c148a308867dcdf
NathanielWatife <awosanminathaniel0@gmail.com>
2024-01-25 23:38:38 +0100

    new directory

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return (a + b);
+}
+
+const a = parseInt(process.argv[2], 10);
+const b = parseInt(process.argv[3], 10);
+
+if (isNaN(a) || isNaN(b)) {
+
+} else {
+  const result = add(a, b);
+  console.log(result);
+}
