980687eb41f1b002ab55a046e69c8449455e8487
NathanielWatife <awosanminathniel0@gmail.com>
2024-08-05 23:58:26 +0100

    Added path env

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     """Returns a set of all elements present in only one set.
 
@@ -7,4 +8,11 @@ def only_diff_elements(set_1, set_2):
     Returns:
     A set containing all elements present in only one set.
     """
-    return (set_1 ^ set_2)
+    only_diff_set = set()
+    for element in set_1:
+        if element not in set_2:
+            only_diff_set.add(element)
+    for element in set_2:
+        if element not in set_1:
+            only_diff_set.add(element)
+    return only_diff_set
